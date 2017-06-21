15a1aa0a06f418402d562ce823611255bd6b70f5
Kendall Adkins <Kendall@Kendalls-MacBook-Pro.local>
2017-06-21 12:44:37 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,26 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// cards on table
+var cards = ["queen", "queen", "king", "king"];
 
+var cardsInPlay = [];
+
+// user first flip
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push('cardOne');
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+
+cardsInPlay.push('cardTwo');
+console.log("User flipped " + cardTwo);
+
+// if two cards are selected,
+if (cardsInPlay.length === 2) {
+// if they match,
+	if (cardsInPlay[1] === cardsInPlay[1]) {
+		alert("You Found A Match!");
+	} else {
+		alert("Sorry, Try Again.");
+	}
+}
