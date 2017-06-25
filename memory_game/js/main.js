36f9612c93e9cee8d6a55cdc6c93a747e18e5bf4
Kendall <adkins.kendall90@gmail.com>
2017-06-25 11:16:06 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,23 +25,38 @@ var cards = [
 ];
 
 var cardsInPlay = [];
-
 var checkForMatch = function() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You Found A Match!");
-	} else {
-		alert("Sorry, Try Again.");
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You Found A Match!");
+		} else {
+			alert("Sorry, Try Again.");
+		}
 	}
 };
 
-var flipCard = function(cardId) {
+
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2);
+	checkForMatch();
 };
 
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
 
-flipCard(0);
-flipCard(2);
-checkForMatch();
+createBoard();
